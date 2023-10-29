6c401f67356ce05c458a82e2ebba0b3196406f33
lubovkast <lyuba-la@mail.ru>
2023-10-29 04:52:25 +0300

    2

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*
+git init- это изначально пустая папка
+git remote add origin tps://github.com/lubovkast/add-js.githt https://github.com/lubovkast/add-js.git// привязываем папку к репозиторию , юерем код ссылки с гита
+git fetch 
+git checkout -b js3 //создаем ветку
+git add .
+git commit -m '...'
+git push origin js3 // имя ветки которую хотим отправить
+клавиша Q это выйтис конца 
+*/
